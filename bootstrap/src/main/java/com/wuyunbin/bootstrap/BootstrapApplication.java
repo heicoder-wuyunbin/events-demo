b7f558e663526c9c9065cae640a3cb57c40a9599
jhc <2390915425@qq.com>
2025-12-07 17:04:37 +0800

    chore(bootstrap): 添加注释标记
    - 在BootstrapApplication类中添加了作者注释标记"//jianhoncai"

--- bootstrap/src/main/java/com/wuyunbin/bootstrap/BootstrapApplication.java
+++ bootstrap/src/main/java/com/wuyunbin/bootstrap/BootstrapApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.wuyunbin")
 public class BootstrapApplication {
-
+//jianhoncai
 	public static void main(String[] args) {
 		SpringApplication.run(BootstrapApplication.class, args);
 	}
