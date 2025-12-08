0dd98eb5e1eeb80b2d1071be32ae3613b724afad
zz <450558397@qq.com>
2025-12-08 23:40:26 +0800

    docs(in-web): 更新会员控制器注释
    - 将类注释中的"张震最近添加的测试3"更新为"张震最近添加的测试4"
    - 保留原有注释内容和作者信息

--- events-demo/adapter/in-web/src/main/java/com/wuyunbin/in/web/controller/MemberController.java
+++ events-demo/adapter/in-web/src/main/java/com/wuyunbin/in/web/controller/MemberController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 /**
- * 张震最近添加的测试3
+ * 张震最近添加的测试4
  * aaaaa
  * @author wuyunbin
  */
