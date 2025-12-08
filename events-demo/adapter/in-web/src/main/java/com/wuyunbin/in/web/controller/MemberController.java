565f4cd128f2cb2cf6d616ac06dc4d5069066d28
zz <450558397@qq.com>
2025-12-08 23:38:27 +0800

    docs(in-web): 更新成员控制器注释
    - 将注释从"张震最近添加的测试2"更新为"张震最近添加的测试3"

--- events-demo/adapter/in-web/src/main/java/com/wuyunbin/in/web/controller/MemberController.java
+++ events-demo/adapter/in-web/src/main/java/com/wuyunbin/in/web/controller/MemberController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 /**
- * 张震最近添加的测试2
+ * 张震最近添加的测试3
  * aaaaa
  * @author wuyunbin
  */
