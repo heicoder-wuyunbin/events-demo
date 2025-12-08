f98c370132956ac83a4862a3db46fc5e41c934be
zz <450558397@qq.com>
2025-12-08 23:05:48 +0800

    docs(in-web): 更新成员控制器的注释说明
    - 将注释从"zz的测试"更新为"zz最近添加的测试"
    - 保持原有注释结构和作者信息不变

--- events-demo/adapter/in-web/src/main/java/com/wuyunbin/in/web/controller/MemberController.java
+++ events-demo/adapter/in-web/src/main/java/com/wuyunbin/in/web/controller/MemberController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 /**
- * zz的测试
+ * zz最近添加的测试
  * aaaaa
  * @author wuyunbin
  */
